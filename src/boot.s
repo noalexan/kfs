1bc20c91ddd5c18d40a10fe8d0b7ec37ba14a6c7
noalexan <noalexan@student.42nice.fr>
2025-01-04 04:15:08 +0100

    Add global descriptor table

--- src/boot.s
+++ src/boot.s
@@ -34,10 +34,40 @@ stack_top:
 .global _start
 .extern kernel_main
 
+gdt_start:
+.long 0x00
+.long 0x00
+
+gdt_code:
+.long 0xffff
+.long 0x00
+.short 0x00
+.short 0b10011010
+.short 0b11001111
+.short 0x00
+
+gdt_data:
+.long 0xffff
+.short 0x00
+.short 0x00
+.short 0b10010010
+.short 0b11001111
+.short 0x00
+
+gdt_end:
+
+gdt_descriptor:
+.long gdt_end - gdt_start - 1
+.long gdt_start
+
 _start:
 	lea esp, stack_top
+	lgdt [gdt_descriptor]
 
+	mov eax, 0b00000001
+	mov cr0, eax
 	cli
+
 	call kernel_main
 
 	hlt
