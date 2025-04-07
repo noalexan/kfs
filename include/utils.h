06f4e04631df79cbc58444e2595a810fdaa6f254
noalexan <noalexan@student.42nice.fr>
2025-04-08 01:32:08 +0200

    Add pre-processor identation rule in `.clang-format`

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #if __has_attribute(__fallthrough__)
-#define fallthrough __attribute__((__fallthrough__))
+# define fallthrough __attribute__((__fallthrough__))
 #else
-#define fallthrough                                                            \
-	do {                                                                       \
-	} while (0) /* fallthrough */
+# define fallthrough                                                           \
+	 do {                                                                      \
+	 } while (0) /* fallthrough */
 #endif
