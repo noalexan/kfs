358abe5351dbfad05562b054d82405c9ddcaecd5
noalexan <noalexan@student.42nice.fr>
2025-04-18 19:23:19 +0200

    Define `isr_*` and `irq_*`

---
+++ src/irq.s
@@ -0,0 +1,59 @@
+.intel_syntax noprefix
+.code32
+
+.section .text
+	.extern interrupt_handler
+
+interrupt_routine:
+	pusha
+
+	xor eax, eax
+	mov ax, ds
+	push eax
+
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	push esp
+	call interrupt_handler
+	pop esp
+
+	pop ebx
+	mov ds, bx
+	mov es, bx
+	mov fs, bx
+	mov gs, bx
+
+	popa
+	add esp, 0x08
+	sti
+	iret
+
+.macro isq_stub num, vector
+.global irq_\num
+irq_\num:
+	cli
+	push 0
+	push \vector
+	jmp interrupt_routine
+.endm
+
+isq_stub  0, 32
+isq_stub  1, 33
+isq_stub  2, 34
+isq_stub  3, 35
+isq_stub  4, 36
+isq_stub  5, 37
+isq_stub  6, 38
+isq_stub  7, 39
+isq_stub  8, 40
+isq_stub  9, 41
+isq_stub 10, 42
+isq_stub 11, 43
+isq_stub 12, 44
+isq_stub 13, 45
+isq_stub 14, 46
+isq_stub 15, 47
--- src/isr.s
+++ src/isr.s
@@ -3,52 +3,81 @@
 
 .section .text
 
+.extern exception_handler
+
 .macro isr_err_stub num
-.global isr_stub_\num
-isr_stub_\num:
+.global isr_\num
+isr_\num:
+	cli
 	push \num
-	call exception_handler
-	iret
+	jmp exception_routine
 .endm
 
 .macro isr_no_err_stub num
-.global isr_stub_\num
-isr_stub_\num:
+.global isr_\num
+isr_\num:
+	cli
+	push 0
 	push \num
-	call exception_handler
-	iret
+	jmp exception_routine
 .endm
 
-# ISRs for exceptions
-isr_no_err_stub 0   # Division by Zero
-isr_no_err_stub 1   # Debug Exception
-isr_no_err_stub 2   # Non-Maskable Interrupt (NMI)
-isr_no_err_stub 3   # Breakpoint Exception
-isr_no_err_stub 4   # Overflow Exception
-isr_no_err_stub 5   # Bounds Check Exception
-isr_no_err_stub 6   # Invalid Opcode Exception
-isr_no_err_stub 7   # Device Not Available Exception
-isr_err_stub    8   # Double Fault
-isr_no_err_stub 9   # Coprocessor Segment Overrun (386 or earlier only)
-isr_err_stub    10  # Invalid Task State Segment
+exception_routine:
+	pusha
+
+	mov ax, ds
+	push eax
+
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	call exception_handler
+
+	pop eax
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	popa
+
+	add esp, 8
+
+	sti
+	iret
+
+isr_no_err_stub  0  # Division by Zero
+isr_no_err_stub  1  # Debug Exception
+isr_no_err_stub  2  # Non-Maskable Interrupt
+isr_no_err_stub  3  # Breakpoint
+isr_no_err_stub  4  # Overflow
+isr_no_err_stub  5  # Bounds Check
+isr_no_err_stub  6  # Invalid Opcode
+isr_no_err_stub  7  # Device Not Available
+isr_err_stub     8  # Double Fault
+isr_no_err_stub  9  # Coprocessor Segment Overrun
+isr_err_stub    10  # Invalid TSS
 isr_err_stub    11  # Segment Not Present
 isr_err_stub    12  # Stack Fault
 isr_err_stub    13  # General Protection Fault
 isr_err_stub    14  # Page Fault
-isr_no_err_stub 15  # Reserved Exception
-isr_no_err_stub 16  # x87 Floating-Point Exception
-isr_err_stub    17  # Alignment Check Exception (486+)
-isr_no_err_stub 18  # Machine Check Exception (Pentium+)
-isr_no_err_stub 19  # SIMD Floating-Point Exception (SSE)
-isr_no_err_stub 20  # Virtualization Exception (Intel VT-x)
-isr_no_err_stub 21  # Control Protection Exception
-isr_no_err_stub 22  # \
-isr_no_err_stub 23  # |
-isr_no_err_stub 24  # |
-isr_no_err_stub 25  # |
-isr_no_err_stub 26  # | reserved
-isr_no_err_stub 27  # | exceptions
-isr_no_err_stub 28  # |
-isr_no_err_stub 29  # |
-isr_err_stub    30  # |
-isr_no_err_stub 31  # /
+isr_no_err_stub 15  # Reserved
+isr_no_err_stub 16  # x87 Floating-Point
+isr_err_stub    17  # Alignment Check
+isr_no_err_stub 18  # Machine Check
+isr_no_err_stub 19  # SIMD Floating-Point
+isr_no_err_stub 20  # Virtualization
+isr_no_err_stub 21  # Control Protection
+isr_no_err_stub 22  # Reserved
+isr_no_err_stub 23  # Reserved  
+isr_no_err_stub 24  # Reserved
+isr_no_err_stub 25  # Reserved
+isr_no_err_stub 26  # Reserved
+isr_no_err_stub 27  # Reserved
+isr_no_err_stub 28  # Reserved
+isr_no_err_stub 29  # Reserved
+isr_err_stub    30  # Security Exception
+isr_no_err_stub 31  # Reserved
