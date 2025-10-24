ee6a07094897eec83e5ffe1bab993609a97c91dd
jbettini <ashhxtm@gmail.com>
2025-10-24 03:22:26 +0200

    dev(kernel): move init function from main to x86.c

--- src/arch/x86/x86.c
+++ src/arch/x86/x86.c
@@ -1,6 +1,10 @@
 #include <drivers/tty.h>
+#include <drivers/keyboard.h>
 #include <kernel/mb2_info.h>
 #include <memory/memory.h>
+#include <memory/slab.h>
+#include <memory/buddy.h>
+#include <memory/vmalloc.h>
 #include <memory/page.h>
 #include <memory/vmm.h>
 #include <x86.h>
@@ -10,14 +14,15 @@ __attribute__((section(".stack"))) uint8_t kernel_stack[16 * 1024] __attribute__
 void x86_init(void)
 {
 
-	ttys_init();
 	gdt_init();
 	idt_init();
 	idt_register_interrupt_handlers(14, (irqHandler)page_fault_handler);
-
 	mb2_init();
 	vmm_finalize();
-	// Cleanup here
-	// paging_cleanup();
 	page_descriptor_init();
+	buddy_init();
+	slab_init();
+	vmalloc_init();
+	keyboard_init();
+	ttys_init();
 }
